2c54a35b6f0be104da47121e3514ba582c68f37c
Karol Giruc <kgiruc76@gmail.com>
2023-01-08 11:48:14 +0100

    fonts custom

--- smartdom/src/main.jsx
+++ smartdom/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './components/Home.jsx'
 import './style/index.css'
-import './style/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
