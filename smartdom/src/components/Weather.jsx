ee412bced9ac27fdac67d9e04e5d1a14739351b4
Karol Giruc <kgiruc76@gmail.com>
2023-01-09 12:03:33 +0100

    weather widget

--- smartdom/src/components/Weather.jsx
+++ smartdom/src/components/Weather.jsx
@@ -9,18 +9,35 @@ function Weather() {
 
 
     useEffect(() => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=Gdansk&units=imperial&APPID=${api.key}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=strzałkowo&units=metric&APPID=${api.key}`)
             .then(res => res.json())
             .then(result => {
                 setWeather(result)
-                console.log(result)
             })
-    })
+    }, [])
 
 
     return (
         <div className="weather-data">
-            <p>{weather.name}</p>
+            {
+                weather.main ? (
+                    <div className="weather-data">
+                        <p>{weather.name}</p>
+                        <img className="weather-image"
+                             src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                             alt="weather_icon"/>
+                        <p className="temp">{Math.round(weather.main.temp)}°C</p>
+                        <p>{weather.weather[0].main}</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p className="inscription">chuj w dupe</p>
+                    </div>
+                )}
+
+            {weather.cod === "404" && (
+                <p>Error</p>
+            )}
         </div>
     );
 }
