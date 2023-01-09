2013bb52bb3779444061eca257ab9729ff563226
Karol Giruc <kgiruc76@gmail.com>
2023-01-09 14:35:02 +0100

    weather widget api

--- smartdom/src/components/Weather.jsx
+++ smartdom/src/components/Weather.jsx
@@ -1,9 +1,9 @@
 import {useEffect, useState} from "react";
+import gif_loading from "../assets/gif/loading.gif";
 
 function Weather() {
     const [weather, setWeather] = useState([{}]);
     const [loading, setLoading] = useState(true);
-
 
     useEffect(() => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=strzałkowo&lang=pl&units=metric&APPID=${import.meta.env.VITE_KEY}`)
@@ -12,18 +12,18 @@ function Weather() {
                 setWeather(result);
                 setLoading(false);
             })
-            .catch(e =>{
+            .catch(e => {
                 console.log(e);
             })
     }, [])
-
 
     return (
         <div className="text-white text-3xl font-rubik float-right p-10 w-1/2">
 
             {
                 weather.main ? (
-                    <div className="grid grid-rows-2 grid-cols-2 gap-3 text-center justify-center items-center border-2 border-blue-500 p-5 rounded-2xl">
+                    <div
+                        className="grid grid-rows-2 grid-cols-2 gap-3 text-center justify-center items-center border-2 border-blue-500 p-5 rounded-2xl">
                         <p className="text">{Math.round(weather.main.temp)}°C</p>
                         <img className="ml-auto mr-auto items-stretch row-span-2 w-1/2"
                              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
@@ -31,8 +31,8 @@ function Weather() {
                         <p className="text-lg">{weather.weather[0].description}</p>
                     </div>
                 ) : (
-                    <div className="">
-                        {loading && <p className="">Loading...</p>}
+                    <div className="w-1/2">
+                        {loading && <img src={gif_loading} className="w-1/2 h-1/2" alt="loading"/>}
                     </div>
                 )}
         </div>
