a97f833547c6cfbf0d35f6a4c76ca325d5bb3d64
Karol Giruc <kgiruc76@gmail.com>
2023-01-17 16:23:17 +0100

    fireabase database add

--- smartdom/data/firebase-init.js
+++ smartdom/data/firebase-init.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBa-ith4CzHcFqiFHJxKiHo5RVVlv7hwVM",
-    authDomain: "dbmicro-84106.firebaseapp.com",
-    databaseURL: "https://dbmicro-84106-default-rtdb.firebaseio.com",
-    projectId: "dbmicro-84106",
-    storageBucket: "dbmicro-84106.appspot.com",
-    messagingSenderId: "774473211335",
-    appId: "1:774473211335:web:b5c4d485307e3e2bbf0de2"
+    apiKey: import.meta.env.VITE_API,
+    authDomain: import.meta.env.VITE_DOMAIN,
+    databaseURL: import.meta.env.VITE_URL,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 const app = initializeApp(FIREBASE_CONFIG);
